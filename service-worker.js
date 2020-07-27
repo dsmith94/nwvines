8809f38870fc8758b789271488bbaef2e69e6fe5
Donald Smith <dsmith94@gmail.com>
2020-07-26 22:39:17 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -55,7 +55,7 @@ self.addEventListener('fetch', (evt) => {
           .catch(() => {
             return caches.open(CACHE_NAME)
                 .then((cache) => {
-                  return cache.match('offline.html');
+                  return cache.match('https://dsmith94.github.io/nwvines/offline.html');
                 });
           })
   );
